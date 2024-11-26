9fccb76ba9831cbd2431d90e3e2da69f353de6ff
Dechamps Chen <dechamps.chen@gmail.com>
2024-11-26 10:16:32 +0100

    useMangaList

--- src/core/functions/useMangaList.ts
+++ src/core/functions/useMangaList.ts
@@ -1,45 +1,152 @@
 import axios from "axios";
 import { IMangaResult, ISearch } from "@consumet/extensions"
 
-type fetchType = "[latestUploadedChapter]" | "[latestUploadedChapter]" | "[latestUploadedChapter]"
-
-type UseMangaListReturnType = { fetchMangaList: (type: fetchType) => Promise<ISearch<IMangaResult> | undefined> }
-
-async function fetchCoverImage(coverId: string) {
-  const { data } = await axios.get(`https://api.mangadex.org/cover/${coverId}`);
-  const fileName = data.data.attributes.fileName;
-  return fileName;
+type UseMangaListReturnType = {
+  fetchLatestUpdates: (page?: number, limit?: number, familySafe?: boolean) => Promise<ISearch<IMangaResult>>,
+  fetchRecentlyAdded: (page?: number, limit?: number, familySafe?: boolean) => Promise<ISearch<IMangaResult>>,
+  fetchPopular: (page?: number, limit?: number, familySafe?: boolean) => Promise<ISearch<IMangaResult>>,
 }
 
-export function useMangaList(): UseMangaListReturnType | null {
+export function useMangaList(): UseMangaListReturnType {
+  const baseUrl = 'https://mangadex.org';
+  const apiUrl = 'https://api.mangadex.org';
+  
+  async function fetchCoverImage(coverId: string) {
+    const { data } = await axios.get(`${apiUrl}/cover/${coverId}`);
+    const fileName = data.data.attributes.fileName;
+    return fileName;
+  }
 
-  async function fetchMangaList(type: fetchType, page = 1, limit = 20): Promise<ISearch<IMangaResult> | undefined> {
-    const res = await axios.get(`https://api.mangadex.org/manga?order${type}=desc&limit=${limit}&offset=${limit * (page - 1)}&contentRating%5B%5D=safe`);
-    if (res.data.result === 'ok') {
-      const results: ISearch<IMangaResult> = {
-        currentPage: page,
-        results: [],
-      };
-      for (const manga of res.data.data) {
-        const findCoverArt = manga.relationships.find((item: any) => item.type === 'cover_art');
-        const coverArtId = findCoverArt ? findCoverArt.id : null;
-        const coverArt = await fetchCoverImage(coverArtId === null || coverArtId === void 0 ? void 0 : coverArtId);
-        results.results.push({
-          id: manga.id,
-          title: Object.values(manga.attributes.title)[0],
-          altTitles: manga.attributes.altTitles,
-          description: Object.values(manga.attributes.description)[0],
-          status: manga.attributes.status,
-          releaseDate: manga.attributes.year,
-          contentRating: manga.attributes.contentRating,
-          lastVolume: manga.attributes.lastVolume,
-          lastChapter: manga.attributes.lastChapter,
-          image: `https://api.mangadex.org/covers/${manga.id}/${coverArt}`,
-        });
+  async function fetchLatestUpdates(page = 1, limit = 20, familySafe = true): Promise<ISearch<IMangaResult>> {
+    if (page <= 0){
+      throw new Error('Page number must be greater than 0');
       }
-      return results;
+    if (limit > 100){
+      throw new Error('Limit must be less than or equal to 100');
+    }
+    if (limit * (page - 1) >= 10000){
+      throw new Error('not enough results');
+    }
+    try {
+      const res = await axios.get(`${apiUrl}/manga?order[latestUploadedChapter]=desc&limit=${limit}&offset=${limit * (page - 1)}${familySafe ? "&contentRating%5B%5D=safe" : ""}`);
+      
+      if (res.data.result === 'ok') {
+        const results: ISearch<IMangaResult> = {
+          currentPage: page,
+          results: [],
+        };
+        for (const manga of res.data.data) {
+          const findCoverArt = manga.relationships.find((item: any) => item.type === 'cover_art');
+          const coverArtId = findCoverArt ? findCoverArt.id : null;
+          const coverArt = await fetchCoverImage(coverArtId === null || coverArtId === void 0 ? void 0 : coverArtId);
+          results.results.push({
+            id: manga.id,
+            title: String(Object.values(manga.attributes.title)[0]),
+            altTitles: manga.attributes.altTitles,
+            description: String(Object.values(manga.attributes.description)[0]),
+            status: manga.attributes.status,
+            releaseDate: manga.attributes.year,
+            contentRating: manga.attributes.contentRating,
+            lastVolume: manga.attributes.lastVolume,
+            lastChapter: manga.attributes.lastChapter,
+            image: `${baseUrl}/covers/${manga.id}/${coverArt}`,
+          });
+        }
+        return results;
+      }
+      throw new Error(res.data.message);
+    }
+    catch (err: any) {
+      throw new Error(err.message);
     }
   }
 
-  return { fetchMangaList }
+
+  async function fetchRecentlyAdded(page = 1, limit = 20, familySafe = true): Promise<ISearch<IMangaResult>> {
+    if (page <= 0){
+      throw new Error('Page number must be greater than 0');
+      }
+    if (limit > 100){
+      throw new Error('Limit must be less than or equal to 100');
+    }
+    if (limit * (page - 1) >= 10000){
+      throw new Error('not enough results');
+    }
+    try {
+        const res = await axios.get(`${apiUrl}/manga?includes[]=cover_art&contentRating[]=safe${!familySafe ? "&contentRating[]=suggestive&contentRating[]=erotica" : ""}&order[createdAt]=desc&hasAvailableChapters=true&limit=${limit}&offset=${limit * (page - 1)}`);
+        if (res.data.result === 'ok') {
+            const results: ISearch<IMangaResult> = {
+                currentPage: page,
+                results: [],
+            };
+            for (const manga of res.data.data) {
+                const findCoverArt = manga.relationships.find((item: any) => item.type === 'cover_art');
+                const coverArtId = findCoverArt ? findCoverArt.id : null;
+                const coverArt = await fetchCoverImage(coverArtId === null || coverArtId === void 0 ? void 0 : coverArtId);
+                results.results.push({
+                    id: manga.id,
+                    title: String(Object.values(manga.attributes.title)[0]),
+                    altTitles: manga.attributes.altTitles,
+                    description: String(Object.values(manga.attributes.description)[0]),
+                    status: manga.attributes.status,
+                    releaseDate: manga.attributes.year,
+                    contentRating: manga.attributes.contentRating,
+                    lastVolume: manga.attributes.lastVolume,
+                    lastChapter: manga.attributes.lastChapter,
+                    image: `${baseUrl}/covers/${manga.id}/${coverArt}`,
+                });
+            }
+            return results;
+        }
+        throw new Error(res.data.message);
+      }
+      catch (err: any) {
+        throw new Error(err.message);
+      }
+    }
+
+    async function fetchPopular(page = 1, limit = 20, familySafe = true): Promise<ISearch<IMangaResult>> {
+      if (page <= 0){
+        throw new Error('Page number must be greater than 0');
+        }
+      if (limit > 100){
+        throw new Error('Limit must be less than or equal to 100');
+      }
+      if (limit * (page - 1) >= 10000){
+        throw new Error('not enough results');
+      }
+      try{
+          const res = await axios.get(`${apiUrl}/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe${!familySafe ? "&contentRating[]=suggestive" : ""}&hasAvailableChapters=true&limit=${limit}&offset=${limit * (page - 1)}`);
+          if (res.data.result === 'ok') {
+              const results: ISearch<IMangaResult> = {
+                  currentPage: page,
+                  results: [],
+              };
+              for (const manga of res.data.data) {
+                  const findCoverArt = manga.relationships.find((item: any) => item.type === 'cover_art');
+                  const coverArtId = findCoverArt ? findCoverArt.id : null;
+                  const coverArt = await fetchCoverImage(coverArtId === null || coverArtId === void 0 ? void 0 : coverArtId);
+                  results.results.push({
+                      id: manga.id,
+                      title: String(Object.values(manga.attributes.title)[0]),
+                      altTitles: manga.attributes.altTitles,
+                      description: String(Object.values(manga.attributes.description)[0]),
+                      status: manga.attributes.status,
+                      releaseDate: manga.attributes.year,
+                      contentRating: manga.attributes.contentRating,
+                      lastVolume: manga.attributes.lastVolume,
+                      lastChapter: manga.attributes.lastChapter,
+                      image: `${baseUrl}/covers/${manga.id}/${coverArt}`,
+                  });
+              }
+              return results;
+          }
+          throw new Error(res.data.message);
+        }
+        catch (err: any) {
+          throw new Error(err.message);
+        }
+      }
+
+  return { fetchLatestUpdates, fetchRecentlyAdded, fetchPopular }
 }
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,4 @@
 import MangaList from '@/components/MangaList';
-import { Welcome } from '../components/Welcome/Welcome';
 import { Stack } from '@mantine/core';
 import { IMangaResult, ISearch, MANGA } from "@consumet/extensions"
 import { useEffect, useState } from 'react';
@@ -7,10 +6,10 @@ import MangaListSkeleton from '@/components/MangaListSkeleton';
 import { useOutletContext } from 'react-router-dom';
 import { useDocumentTitle } from '@mantine/hooks';
 import { useTranslation } from '@/core/functions/useTranslation';
-import axios, { AxiosResponse } from "axios"
+import { useMangaList } from '@/core/functions/useMangaList';
 
 export function HomePage() {
-  const mangadex = new MANGA.MangaDex;
+  // const mangadex = new MANGA.MangaDex;
   const [lastestUpdates, setLastestUpdates] = useState<ISearch<IMangaResult> | null>(null);
   const [lastestUpdatesIsLoading, setLastestUpdatesIsLoading] = useState<boolean>(true);
   const [popular, setPopular] = useState<ISearch<IMangaResult> | null>(null);
@@ -20,51 +19,14 @@ export function HomePage() {
   const { language, familySafe } = useOutletContext<{ language: string, familySafe: boolean }>();
   const { t } = useTranslation(language);
   useDocumentTitle("Manga - Read unlimited free manga online");
+  const { fetchLatestUpdates, fetchPopular, fetchRecentlyAdded } = useMangaList();
 
   useEffect(() => {
     setLastestUpdatesIsLoading(true);
     setPopularIsLoading(true);
     setRecentlyAddedIsLoading(true);
 
-    async function fetchCoverImage(coverId: string) {
-      const { data } = await axios.get(`https://api.mangadex.org/cover/${coverId}`);
-      const fileName = data.data.attributes.fileName;
-      return fileName;
-    }
-    fetchA().then(v => console.log(v));
-    async function fetchA(limit = 20, page = 1) {
-
-      const res = await axios.get(`https://api.mangadex.org/manga?order[latestUploadedChapter]=desc&limit=20&offset=0&contentRating%5B%5D=safe`);
-      if (res.data.result === 'ok') {
-        const results: { currentPage: number, results: any } = {
-          currentPage: page,
-          results: [],
-        };
-        for (const manga of res.data.data) {
-          const findCoverArt = manga.relationships.find((item: any) => item.type === 'cover_art');
-          const coverArtId = findCoverArt ? findCoverArt.id : null;
-          const coverArt = await fetchCoverImage(coverArtId === null || coverArtId === void 0 ? void 0 : coverArtId);
-          results.results.push({
-            id: manga.id,
-            title: Object.values(manga.attributes.title)[0],
-            altTitles: manga.attributes.altTitles,
-            description: Object.values(manga.attributes.description)[0],
-            status: manga.attributes.status,
-            releaseDate: manga.attributes.year,
-            contentRating: manga.attributes.contentRating,
-            lastVolume: manga.attributes.lastVolume,
-            lastChapter: manga.attributes.lastChapter,
-            image: `https://api.mangadex.org/covers/${manga.id}/${coverArt}`,
-          });
-        }
-        return results;
-      }
-    }
-
-    axios.get("https://api.mangadex.org/manga?limit=10&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5BlatestUploadedChapter%5D=desc")
-      .then(v => console.log("axios: ", v))
-
-    mangadex.fetchLatestUpdates(1, 20)
+    fetchLatestUpdates(1, 20, familySafe)
       .then(v => {
         setLastestUpdates(v);
       })
@@ -75,33 +37,32 @@ export function HomePage() {
         setLastestUpdatesIsLoading(false);
       });
 
-    // mangadex.fetchPopular(1, 20)
-    //   .then(v => {
-    //     setPopular(v);
-    //   })
-    //   .catch(() => {
-    //     setPopular(null);
-    //   })
-    //   .finally(() => {
-    //     setPopularIsLoading(false);
-    //   });
+      fetchPopular(1, 20, familySafe)
+      .then(v => {
+        setPopular(v);
+      })
+      .catch(() => {
+        setPopular(null);
+      })
+      .finally(() => {
+        setPopularIsLoading(false);
+      });
 
-    // mangadex.fetchRecentlyAdded(1, 20)
-    //   .then(v => {
-    //     setRecentlyAdded(v);
-    //   })
-    //   .catch(() => {
-    //     setRecentlyAdded(null);
-    //   })
-    //   .finally(() => {
-    //     setRecentlyAddedIsLoading(false);
-    //   });
+      fetchRecentlyAdded(1, 20, familySafe)
+      .then(v => {
+        setRecentlyAdded(v);
+      })
+      .catch(() => {
+        setRecentlyAdded(null);
+      })
+      .finally(() => {
+        setRecentlyAddedIsLoading(false);
+      });
 
   }, [familySafe]);
 
   return (
     <Stack w={{ base: "100%", md: 1080 }} py={20} gap="2.5rem">
-      {/* <Welcome /> */}
       {
         (lastestUpdates && !lastestUpdatesIsLoading) ?
           <MangaList title={t("lastestUpdates")} list={lastestUpdates} />
